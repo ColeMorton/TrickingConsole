cd9610acd3004d96bca73d17f7053ca83cfca6a8
ColeMorton <Cole.Morton@charmhealth.com.au>
2012-10-03 16:50:20 +1000

    Migrations

---
+++ CodeFirstMigrations/CodeFirstMigrations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Data;
+
+namespace CodeFirstMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trickers = new TrickingContext().Trickers;
+
+            foreach (var tricker in trickers)
+            {
+                Console.WriteLine(tricker.Name);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeFirstMigrations/Data/TrickersTrick.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+    public class TrickersTrick
+    {
+        public int Id { get; set; }
+        public int TrickId { get; set; }
+        public int TrickerId { get; set; }
+        public int Profiency { get; set; }
+    }
+}
