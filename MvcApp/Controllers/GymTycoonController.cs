adac4b7df41f843a54f64bab3bfb0a7293760da3
Cole Morton <cole.morton@hotmail.com>
2013-02-11 22:25:03 +1000

    Started Gym Tycoon

---
+++ MvcApp/Controllers/GymTycoonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tricking.Mvc.Controllers
+{
+    public class GymTycoonController : Controller
+    {
+        //
+        // GET: /GymTycoon/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WorldMap()
+        {
+            return View();
+        }
+
+    }
+}
