577f6bd6696b215e758af0bbe1cbdae8ba866b20
ColeMorton <Cole.Morton@charmhealth.com.au>
2013-02-12 12:43:15 +1000

    Added GymTycoon project

---
+++ GymTycoon/Controllers/NewGymController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace GymTycoon.Controllers
+{
+    public class NewGymController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
