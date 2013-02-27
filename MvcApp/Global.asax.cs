097229cf0ad7bc9ef2774eff286fe997ee37f6c0
ColeMorton <Cole.Morton@charmhealth.com.au>
2013-02-27 11:09:16 +1000

    Update all nuget packages
    Added Tricking.Database project to solution

--- MvcApp/Global.asax.cs
+++ MvcApp/Global.asax.cs
@@ -1,17 +1,10 @@
-﻿using System.Configuration;
-using System.Data.Entity;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Tricking.Domain;
 using Tricking.Mvc.App_Start;
-using Tricking.Mvc.Infrastructure;
 
 namespace Tricking.Mvc
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
---
+++ Tricking.Database/Program.cs
@@ -0,0 +1,13 @@
+﻿//This project will be used to create and seed the database for use within this solution
+//Code first migrations using powershell
+//http://msdn.microsoft.com/en-US/data/jj591621
+
+namespace Tricking.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
